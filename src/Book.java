3086e589694f6bf5d09a9b621831763a4e75496b
Vanpe1t <makarov.maks.v@yandex.ru>
2022-11-27 14:24:37 +0300

    hometask object's methods

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private final String name;
     private final Author author;
@@ -8,6 +10,19 @@ public class Book {
                 ", author=" + author +
                 ", yearOfPublication=" + yearOfPublication +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearOfPublication == book.yearOfPublication && Objects.equals(name, book.name) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, author, yearOfPublication);
     }
 
     public Book(String name, Author author){
